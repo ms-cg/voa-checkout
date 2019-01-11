52ee8ae4acdc8f67f230bf45c2d6792487a30d02
ms-cg <matthew.seymour@capgemini.com>
2019-01-11 10:40:04 +0000

    Project creation. Simple unit tests added. Stubbed program code added.

---
+++ VoaCheckoutApp/VoaCheckout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VoaCheckout
+{
+    /// <summary>
+    /// Simple 'checkout' class for calculating the total combined price of items.
+    /// </summary>
+    public class VoaCheckout
+    {
+        /// <summary>
+        /// Calucate the combined total price of the given items.
+        /// </summary>
+        /// <param name="items">Arrary of items represented as Strings.
+        /// Permitted values are "Apple" and "Orange".
+        /// </param>
+        /// <returns>Total price of the items.</returns>
+        public static int GetTotalPrice(String[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VoaCheckoutTest/VoaCheckoutUnitTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VoaCheckoutTest
+{
+    /// <summary>
+    /// Unit tests for the VoaCheckout class; just one public method GetTotalPrice().
+    /// </summary>
+    [TestClass]
+    public class VoaCheckoutUnitTest
+    {
+        /// <summary>
+        /// Perform the 'act' and 'assert' processes common to all unit tests in this class.
+        /// </summary>
+        /// <param name="items">The items to pass as the input parameter to the GetTotalPrice() method.</param>
+        /// <param name="expectation">The expected result.</param>
+        public void CommonActAssert(String[] items, int expectation)
+        {
+            // Act
+            int totalPrice = VoaCheckout.VoaCheckout.GetTotalPrice(items);
+
+            // Assert
+            Assert.AreEqual(expectation, totalPrice);
+        }
+
+        [TestMethod]
+        public void OneAppleIs60p()
+        {
+            // Arrange
+            String[] items = { "Apple" };
+            int expectation = 60;
+
+            // Act, Assert
+            CommonActAssert(items, expectation);
+        }
+
+        [TestMethod]
+        public void TwoApplesAre120p()
+        {
+            // Arrange
+            String[] items = { "Apple", "Apple" };
+            int expectation = 120;
+
+            // Act, Assert
+            CommonActAssert(items, expectation);
+        }
+
+        [TestMethod]
+        public void OneOrangeIs25p()
+        {
+            // Arrange
+            String[] items = { "Orange" };
+            int expectation = 25;
+
+            // Act, Assert
+            CommonActAssert(items, expectation);
+        }
+
+        [TestMethod]
+        public void TwoOrangesAre50p()
+        {
+            // Arrange
+            String[] items = { "Orange", "Orange" };
+            int expectation = 50;
+
+            // Act, Assert
+            CommonActAssert(items, expectation);
+        }
+
+        [TestMethod]
+        public void OneAppleAndOneOrangeIs85p()
+        {
+            // Arrange
+            String[] items = { "Apple", "Orange" };
+            int expectation = 85;
+
+            // Act, Assert
+            CommonActAssert(items, expectation);
+        }
+    }
+}
